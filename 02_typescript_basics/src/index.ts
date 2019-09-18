9b821c8bb37e4a15f3e07956239983521b46e1cf
Girma Nigusse <xgirma@gmail.com>
2019-09-17 21:32:14 -0400

    interface

--- 02_typescript_basics/src/index.ts
+++ 02_typescript_basics/src/index.ts
@@ -1,24 +1,55 @@
-// Example: simple array
+// 1. Example: simple array
 let aa: number[] = [];
 aa.push(33);
 // aa.push("abc"); // error TS2345: Argument of type '"abc"' is not assignable to parameter of type 'number'.
 
-// Example: array of never
+// 2. Example: array of never
 let bb = []; // equivalent to let bb: never[]
 bb.push(66);
-bb.push('xyz');
+bb.push("xyz");
 
-// Example: element
+// 3. Example: element
 let cc = [9];
 cc.push(100);
 // cc.push('pop'); // error TS2345: Argument of type '"abc"' is not assignable to parameter of type 'number'.
 
-// Example: tupple with fixed length
+// 4. Example: tupple with fixed length
 let address: [number, string, string, number] = [
-    123,
-    "Fake Street",
-    "Nowhere, USA",
-    10110
+  123,
+  "Fake Street",
+  "Nowhere, USA",
+  10110
 ];
 
 // address = [1, 2, 3, 4]; // Type 'number' is not assignable to type 'string'.
+
+// 5. Example: object type
+let ll : { houseNumber: number, streetNumber: number };
+
+ll = {
+    houseNumber: 3,
+    streetNumber: 1533
+};
+
+// ll = {
+//     streetNumber: 1533
+// };
+// error TS2741: Property 'houseNumber' is missing in type '{ streetNumber: number; }' but required in type '{ houseNumber: number; streetNumber: number; }'.
+
+// 6. Example: optional operator (?) to
+let m : { make: string, model?: number };
+
+m = {
+    make: "BMW"
+};
+
+// 7. Example: interface
+interface Loc {
+    longitude: number;
+    latitude: number;
+}
+
+let lll : Loc = {
+    longitude: 1242142342,
+    latitude: 2352525
+};
