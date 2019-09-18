4280e5665911814d44066182fe2fbee175f81721
Girma Nigusse <xgirma@gmail.com>
2019-09-17 21:58:24 -0400

    intersection and union

--- 02_typescript_basics/src/index.ts
+++ 02_typescript_basics/src/index.ts
@@ -53,3 +53,37 @@ let lll : Loc = {
     longitude: 1242142342,
     latitude: 2352525
 };
+
+// 8. Example: Intersection type
+interface HasPhoneNumber {
+    name: string;
+    phone: number;
+}
+
+interface HasEmail {
+    name: string;
+    email: string;
+}
+
+let contactInfo: HasEmail | HasPhoneNumber = Math.random() > 0.5
+    ? {
+        name: "Mike",
+        phone: 1231212343242343
+    }
+    : {
+        name: "Mike",
+        email: "mike@example.com"
+    };
+
+console.log(contactInfo.name);
+// console.log(contactInfo.phone); // error TS2339: Property 'phone' does not exist on type 'HasPhoneNumber | HasEmail'.
+// console.log(contactInfo.email); // error TS2339: Property 'email' does not exist on type 'HasPhoneNumber | HasEmail'.
+
+// 9. Example: Union type
+let otherContactInfo : HasEmail & HasPhoneNumber = {
+    name: "Mike",
+    email: "mike@example.com",
+    phone: 24234234234234
+};
+
+console.log(otherContactInfo);
